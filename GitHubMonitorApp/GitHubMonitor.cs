7f4ebfc237a5f6a56cb56cbb80bed84e8a980b28
Javier Rivera <ingjrz@gmail.com>
2025-01-13 22:17:46 -0600

    succeed at git push and trigger the function

--- GitHubMonitorApp/GitHubMonitor.cs
+++ GitHubMonitorApp/GitHubMonitor.cs
@@ -22,15 +22,15 @@ public static class GitHubMonitor
         string name = req.Query["name"];
 
         string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-        dynamic data = JsonConvert.DeserializeObject<Rootobject>(requestBody); 
-        name = name ?? data?.name;
-
-        //TODO: Do something with the data.
-        log.LogInformation(requestBody); 
+        dynamic data = JsonConvert.DeserializeObject(requestBody);
+        name = name ?? data?.name;        
 
         string responseMessage = string.IsNullOrEmpty(name)
             ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            : $"Hello, {name}. This HTTP triggered function executed successfully."; 
+            : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+        //TODO: Do something with the data.
+        log.LogInformation(responseMessage);
 
         return new OkObjectResult(responseMessage);
     }
