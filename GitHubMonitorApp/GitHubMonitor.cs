e30605eb286755107da72754b4cab33bae468bc3
Javier Rivera <ingjrz@gmail.com>
2025-01-13 21:42:04 -0600

    test1

--- GitHubMonitorApp/GitHubMonitor.cs
+++ GitHubMonitorApp/GitHubMonitor.cs
@@ -30,7 +30,7 @@ public static class GitHubMonitor
 
         string responseMessage = string.IsNullOrEmpty(name)
             ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            : $"Hello, {name}. This HTTP triggered function executed successfully."; 
 
         return new OkObjectResult(responseMessage);
     }
